88415c68d07aec11cdad8fdf24b8e29b4ca9c4e7
Joseph Randall Hunt <randallhunt@gmail.com>
2011-04-11 04:37:34 -0700

    initial commit

---
+++ trunk/testbed/tourmaker/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
+class Overlay(models.Model): 
+  lat = models.FloatField()
+  lng = models.FloatField()
+  uri = models.URLField()
+  content = models.TextField()
+
+class Placemark(models.Model):
+  lat = models.FloatField()
+  lng = models.FloatField()
+  content = models.TextField()
+
+class Audio(models.Model):
+  uri = models.URLField();
+
+class Camera(models.Model):
+  lat = models.FloatField()
+  lng = models.FloatField()
+  alt = models.FloatField()
+
+class TimelineItem(models.Model):
+  begin = models.TimeField()
+  end = models.TimeField()
+  item_type = models.ForeignKey(ContentType) 
+  item_id = models.PositiveIntegerField() 
+  item = generic.GenericForeignKey('item_type', 'item_id') 
